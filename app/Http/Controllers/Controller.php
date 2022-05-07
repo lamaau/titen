be88b05c3c89655e476ead1ae11f7ec28c4c4a58
rizkhal <lamaaurizkhal@gmail.com>
2022-05-07 17:31:09 +0900

    wip

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,21 +7,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info(
- *     description="Titen API Documentation",
- *     version="1.0.0",
- *     title="Titen API Documentation",
- *     termsOfService="http://swagger.io/terms/",
- *     @OA\Contact(
- *         email="support@titen.org"
- *     ),
- *     @OA\License(
- *         name="Apache 2.0",
- *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
- *     )
- * )
- */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
