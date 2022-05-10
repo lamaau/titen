59f7e58d3e290dc1cbcd21cf7983a6b55580d07b
rizkhal <lamaaurizkhal@gmail.com>
2022-05-11 02:54:51 +0900

    feat: initial module setting

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Modules\Core\Providers\RouteServiceProvider;
 
 class RedirectIfAuthenticated
 {
